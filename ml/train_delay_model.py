5cec15527c98436454103c4d9a5983736eb875a4
mohammadSous <mohammad4osu@gmail.com>
2026-05-31 05:25:01 +0300

    used logistic regression after encoding and scaling the data

--- ml/train_delay_model.py
+++ ml/train_delay_model.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 import pandas as pd
-
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report
+)
 from utils.db_connection import get_engine
+
 
 SQL_ML_DIR = Path("sql/ml")
 
-ML_QUERY = {
-    "Delay Dataset": "delay_dataset.sql"
-}
 
-def load_ml_query(query_name):
+def load_ml_query(file_name):
     engine = get_engine()
 
-    file_name = ML_QUERY[query_name]
     query_path = SQL_ML_DIR / file_name
     query = query_path.read_text()
 
@@ -20,16 +25,57 @@ def load_ml_query(query_name):
     return df
 
 
-for query_name in ML_QUERY:
-    df = load_ml_query(query_name)
+# Load ML dataset
+df = load_ml_query("delay_dataset.sql")
 
-    print("\n" + "=" * 60)
-    print(query_name)
-    print("=" * 60)
-    print(df.head(10))
-
-
+# Separate features and target
 X = df.drop("is_delayed", axis=1)
 y = df["is_delayed"]
 
-print(X.dtypes)
+categorical_columns = [
+    "airline",
+    "origin",
+    "destination",
+    "gate_id"
+]
+
+numerical_columns = [
+    "departure_hour",
+    "departure_day_of_week",
+    "tickets_sold",
+    "average_ticket_price",
+    "employee_count",
+    "baggage_count",
+    "total_baggage_weight"
+]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_columns),
+        ("num", StandardScaler(), numerical_columns)
+    ]
+)
+
+X_processed = preprocessor.fit_transform(X)
+
+
+#splitting the data 80/20
+X_train, X_test, y_train, y_test = train_test_split(
+    X_processed,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+#training
+model = LogisticRegression(
+    max_iter=1000,
+    random_state=42,
+    class_weight="balanced"
+)
+
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
